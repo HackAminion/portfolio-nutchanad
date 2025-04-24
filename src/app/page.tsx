825430c872798b59d2db7794eec38bb056d45666
HackAminion <ooaatt.thailand1@gmail.com>
2025-04-24 17:03:32 +0700

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,61 @@
 import Image from "next/image";
+import { FaLinkedinIn,FaFilePdf } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="min-h-screen bg-[#f7f0eb] text-[#0f0f0f] p-8 font-sans">
+      <nav className="flex justify-center gap-12 font-bold text-sm uppercase mb-12">
+        <a href="#">Bio</a>
+        <a href="/portfolio">Portfolio</a>
+        {/* <a href="#">Contact</a> */}
+      </nav>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+      <div className="max-w-5xl mx-auto bg-white p-8 border border-gray-300 grid md:grid-cols-2 gap-8 items-center">
+        <div>
+          <h1 className="text-3xl font-bold mb-1">นุชนาฏ สาริศรี</h1>
+          <h2 className="text-2xl font-semibold mb-1">(Nutchanad Sarisri)</h2>
+          <p className="mb-4">______________________________</p>
+          <p className="text-base mb-1 indent-4">
+          สวัสดีค่ะ ฉันคือเจ้าหน้าที่จัดซื้อจัดจ้างอาวุโส ผู้มีประสบการณ์จากหลากหลายสายงาน
+          พร้อมมุมมองกว้างและทักษะที่หลากหลาย
+          </p>
+          <p className="text-sm">
+          คลิกที่หน้า <strong className="c text-blue-900">"Portfolio"</strong> เพื่อดูข้อมูลเพิ่มเติม หรือ
+          </p>
+          <p className="text-sm mb-6">
+          คลิกที่ <strong className="text-sm text-red-800">"PDF"</strong> เพื่อดาวน์โหลด Resume
+          </p>
+          
+          <div className="flex gap-4 text-2xl">
+            <a 
+              href="https://www.linkedin.com/in/nutchanad-sarisri-231b89262" 
+              aria-label="LinkedIn" 
+              className="hover:text-blue-600 transition-colors duration-300"
+            >
+              <FaLinkedinIn />
+            </a>
+          </div>
+        </div>
+        <div className="w-full h-full relative">
+          <Image
+            src="/bio_v2.svg"
+            alt="Nutchanad Sarisri Portrait"
+            width={600}
+            height={600}
+            className="object-cover w-full h-auto rounded-lg"
+          />
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="/resume-nutchanad.pdf"
+            download
             target="_blank"
             rel="noopener noreferrer"
+            className="absolute bottom-4 right-4 bg-red-600 text-white p-3 rounded-full text-3xl shadow-lg hover:bg-red-700 transition-transform hover:scale-105"
+            aria-label="Download CV PDF"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+            <FaFilePdf />
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,286 @@
+'use client';
+import { useState } from "react";
+
+export default function PortfolioPage() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [imageIndexes, setImageIndexes] = useState([0, 0, 0, 0]);
+
+    const workImages = [
+        ["/kaibarn1.jpg"],
+        [
+            "/thai2.svg",
+            "/thai1.svg", 
+            "/thai3.svg", 
+            "/thai4.svg", 
+            "/thai5.svg", 
+            "/thai6.svg", 
+            "/thai7.svg", 
+            "/thai8.svg", 
+            "/thai9.svg", 
+            "/thai10.svg",  
+        ],
+        [
+            "/chula1.svg", 
+            "/chula2.svg",
+            "/chula3.svg",
+            "/chula4.svg",
+            "/chula5.svg",
+            "/chula6.svg",
+        ],
+        [
+            "scb1.svg", 
+            "scb2.svg",
+            "scb3.svg",
+            "scb4.svg",
+            "scb5.svg",
+        ]
+    ];
+
+    const handleNext = (i: any) => {
+        setImageIndexes((prev) => {
+            const newIndexes = [...prev];
+            newIndexes[i] = (newIndexes[i] + 1) % workImages[i].length;
+            return newIndexes;
+        });
+    };
+
+    const handlePrev = (i: any) => {
+        setImageIndexes((prev) => {
+            const newIndexes = [...prev];
+            newIndexes[i] = (newIndexes[i] - 1 + workImages[i].length) % workImages[i].length;
+            return newIndexes;
+        });
+    };
+    return (
+        <main className="min-h-screen bg-[#f7f0eb] text-[#0f0f0f] p-8 font-sans">
+            <nav className="flex justify-center gap-12 font-bold text-sm uppercase mb-12">
+                <a href="/">Bio</a>
+                <a href="/portfolio">Portfolio</a>
+                {/* <a href="#">Contact</a> */}
+            </nav>
+            <div className="max-w-5xl mx-auto bg-white rounded-xl p-6 md:flex gap-6">
+                <div className="md:w-1/2 border p-4 rounded-lg shadow-sm bg-white space-y-6">
+                    {/* Personal */}
+                    <section>
+                        <h2 className="text-lg font-semibold mb-2">
+                            ประวัติส่วนตัว | Profile
+                        </h2>
+                        <div className="text-sm text-gray-700 leading-relaxed">
+                            <div className="flex">
+                                <span className="w-24 font-medium">ชื่อเต็ม :</span>
+                                <span>นุชนาฏ สาริศรี</span>
+                            </div>
+                            <div className="flex">
+                                <span className="w-24 font-medium">ชื่อเล่น :</span>
+                                <span>ปีใหม่</span>
+                            </div>
+                            <div className="flex">
+                                <span className="w-24 font-medium">วันเกิด :</span>
+                                <span>31 ธันวาคม 2534</span>
+                            </div>
+                            <div className="flex">
+                                <span className="w-24 font-medium">อายุ :</span>
+                                <span>33</span>
+                            </div>
+                            <div className="flex">
+                                <span className="w-24 font-medium">สัญชาติ :</span>
+                                <span>ไทย</span>
+                            </div>
+                            <div className="flex">
+                                <span className="w-24 font-medium">เชื้อชาติ :</span>
+                                <span>ไทย</span>
+                            </div>
+                            <div className="flex">
+                                <span className="w-24 font-medium">ศาสนา :</span>
+                                <span>พุทธ</span>
+                            </div>
+                            <div className="flex">
+                                <span className="w-24 font-medium">อีเมล์ :</span>
+                                <span>nutchanadsarisri34@gmail.com</span>
+                            </div>
+                            <div className="flex">
+                                <span className="w-24 font-medium">เบอร์โทร :</span>
+                                <span>087-945-4044</span>
+                            </div>
+                        </div>
+                    </section>
+                    {/* Skill */}
+                    <section>
+                        <h2 className="text-lg font-semibold mb-2">ทักษะ | Skill</h2>
+                        <ul className="list-none text-gray-700 space-y-2">
+                            <li className="flex items-center gap-8">
+                                <div className="flex items-center gap-5">
+                                    <img src="word.png" alt="Word" className="w-5 h-5" />
+                                    <span className="text-sm font-medium">Word | Docs</span>
+                                </div>
+                                <img
+                                    src="90percen.svg"
+                                    alt="Skill Level"
+                                    className="w-20 h-5"
+                                />
+                            </li>
+                            <li className="flex items-center gap-5">
+                                <div className="flex items-center gap-5">
+                                    <img src="excel.png" alt="Excel" className="w-5 h-5" />
+                                    <span className="text-sm font-medium">Excel | Sheets</span>
+                                </div>
+                                <img
+                                    src="80percen.svg"
+                                    alt="Skill Level"
+                                    className="w-20 h-5"
+                                />
+                            </li>
+                            <li className="flex items-center gap-8">
+                                <div className="flex items-center gap-5">
+                                    <img src="powerpoint.png" alt="PowerPoint" className="w-5 h-5" />
+                                    <span className="text-sm font-medium">Power Point</span>
+                                </div>
+                                <img
+                                    src="80percen.svg"
+                                    alt="Skill Level"
+                                    className="w-20 h-5"
+                                />
+                            </li>
+
+                            <li className="flex items-center gap-21">
+                                <div className="flex items-center gap-5">
+                                    <img src="sap.png" alt="SAP" className="w-5 h-5" />
+                                    <span className="text-sm font-medium">SAP</span>
+                                </div>
+                                <img
+                                    src="80percen.svg"
+                                    alt="Skill Level"
+                                    className="w-20 h-5"
+                                >
+                                </img>
+                            </li>
+                            <li className="flex items-center gap-17.5">
+                                <div className="flex items-center gap-5">
+                                    <img src="canva.png" alt="Canva" className="w-5 h-5" />
+                                    <span className="text-sm font-medium">Canva</span>
+                                </div>
+                                <img
+                                    src="60percen.svg"
+                                    alt="Skill Level"
+                                    className="w-20 h-5"
+                                >
+                                </img>
+                            </li>
+                            <li className="flex items-center gap-13.5">
+                                <div className="flex items-center gap-5">
+                                    <img src="power-bi.png" alt="Power BI" className="w-5 h-5" />
+                                    <span className="text-sm font-medium">Power BI</span>
+                                </div>
+                                <img
+                                    src="50percen.svg"
+                                    alt="Skill Level"
+                                    className="w-20 h-5"
+                                >
+                                </img>
+                            </li>
+                            <li className="flex items-center gap-7.5">
+                                <div className="flex items-center gap-5">
+                                    <img src="chat-gpt.png" alt="ChatGPT" className="w-5 h-5" />
+                                    <span className="text-sm font-medium">ChatGPT (AI)</span>
+                                </div>
+                                <img
+                                    src="50percen.svg"
+                                    alt="Skill Level"
+                                    className="w-20 h-5"
+                                >
+                                </img>
+                            </li>
+                        </ul>
+                    </section>
+                </div>
+                <div className="md:w-1/2 border p-4 rounded-lg shadow-sm bg-white mt-10 md:mt-0">
+                    <h2 className="text-lg font-semibold mb-4">ประวัติการศึกษา | Education</h2>
+                    <div className="space-y-6 border-l-2 border-gray-300 pl-4">
+                        <div>
+                            <span className="block text-sm text-gray-500">
+                                2556 - 2559
+                            </span>
+                            <p className="font-medium text-gray-800">
+                                มหาลัยราชภัฏบ้านสมเด็จเจ้าพระยา
+                            </p>
+                            <p className="text-gray-600 text-sm">
+                                ปริญญาตรี ครุศาสตร์บัณฑิต สาขาจิตวิทยา
+                            </p>
+                        </div>
+                        <div>
+                            <h3 className="text-sm">ใบแสดงผลการเรียน | Transcript</h3>
+                            <p className="mb-1">(<strong className="text-xs text-blue-950">คลิกภาพเพื่อขยาย</strong>)</p>
+                            <img
+                                src="trans-pmai.jpg"
+                                alt="transcript"
+                                className="w-80 h-80 object-cover cursor-pointer transition-transform hover:scale-105 rounded shadow"
+                                onClick={() => setIsOpen(true)}
+                            />
+                            {isOpen && (
+                                <div
+                                    className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50"
+                                    onClick={() => setIsOpen(false)}
+                                >
+                                    <img
+                                        src="/trans-pmai.jpg"
+                                        alt="Zoomed transcript"
+                                        className="max-w-full max-h-full object-contain rounded-lg shadow-lg"
+                                    />
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className="max-w-3xl mx-auto mt-10 bg-white p-6 rounded-lg border">
+                <h2 className="text-lg font-bold mb-6 text-center">ประสบการณ์ทำงาน | Working Experience</h2>
+                <div className="space-y-10 border-l-2 border-gray-300 pl-4">
+                    {[{
+                        year: "01/2568 - ปัจจุบัน",
+                        position: "หัวหน้าฝ่ายจัดซื้อจัดจ้าง",
+                        add: "บริษัท ใกล้บ้าน ใกล้ใจ จํากัด",
+                        desc: "จัดทำงบเสื่อม, จัดซื้ออุปกรณ์สำนักงานทั่วไป - ทางการแพทย์, บริหารจัดการคลินิกในเครือ 20 สาขา, จัดการงานเอกสารภายในบริษัท, จัดจ้างผู้รับเหมาซ่อมแซมสิ่งปลูกสร้าง",
+                    }, {
+                        year: "08/2566 - 07/2567",
+                        position: "เจ้าหน้าที่อาวุโสจัดซื้อจัดจ้าง",
+                        add: "บริษัท ไทยเบฟเวอเรจ โลจิสติกส์ จำกัด",
+                        desc: "งานศูนย์รวมบริการจัดซื้อจัดจ้าง, เปิดPR ในระบบ Coss bay ติดตามการทำรับ PO ในระบบ SAP, ประสานงานการส่งสินค้า, เจรจาทำสัญญางานเช่าต่างๆ, ดูแลสัญญาจ้างเจ้าหน้าพยาบาล-แรงงานต่างๆ, จัดทำ Data ในแต่ละเดือนเพื่อพรีเซนต์, ทำข้อมูลในระบบ PowerBI",
+                    }, {
+                        year: "08/2562 - 11/2566",
+                        position: "เจ้าหน้าที่สำนักงานพัสดุ",
+                        add: "จุฬาลงกรณ์มหาวิทยาลัย",
+                        desc: "ทำการจัดซื้อจัดจ้างยาและเวชภัณฑ์, จัดซื้ออุปกรณ์สำนักงานวัสดุคงคลัง, เบิกเงินยืมรองจ่ายหมุนเวียน, ทำแผนงบประมาณประจำปี, รวบรวมจัดซื้อจัดจ้างที่เป็นมิตรต่อสิ่งแวดล้อมเสนอต่อกรมควบคุมมลพิษ",
+                    }, {
+                        year: "08/2560 - 11/2562",
+                        position: "เลขานุการผู้ช่วยผู้จัดการใหญ่",
+                        add: "สำนักงานใหญ่ธนาคารไทยพาณิชย์ จำกัด (มหาชน)",
+                        desc: "ดูแลตารางผู้บริหาร, ประสานงานภายใน และภายนอกองค์กร, จัดตารางการประชุม, จัดหาห้องประชุมรองรับลูกค้า, ทำเรื่องเบิกจ่ายค่าใช้จ่ายงบประมาณผู้บริหารและทีม, คัดกรอง เอกสารเพื่อเสนอผู้บริหาร, จัดวางแผนการอบรมเทรนนิ่ง",
+                    }].map((item, index) => (
+                        <div key={index}>
+                            <span className="block text-sm text-gray-500">{item.year}</span>
+                            <p className="text-base font-medium text-gray-800">{item.position}</p>
+                            <p className="text-sm text-gray-700">{item.add}</p>
+                            <p className="text-gray-600 text-xs mb-2">{item.desc}</p>
+                            <div className="relative w-full h-48 flex items-center justify-center">
+                                <button
+                                    onClick={() => handlePrev(index)}
+                                    className="absolute left-2 bg-gray-200 rounded-full p-1 hover:bg-gray-300"
+                                >←</button>
+                                <img
+                                    src={workImages[index][imageIndexes[index]]}
+                                    alt=""
+                                    className="h-full object-contain rounded-lg"
+                                />
+                                <button
+                                    onClick={() => handleNext(index)}
+                                    className="absolute right-2 bg-gray-200 rounded-full p-1 hover:bg-gray-300"
+                                >→</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </main>
+    );
+}
